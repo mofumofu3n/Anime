9f5943bb3e3bd2750020b6465f17e7448980c513
mofumofu3n <mofumofu3n@gmail.com>
2014-12-06 02:06:42 +0900

    first commit

---
+++ app/src/main/java/com/ichif1205/anime/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.ichif1205.anime;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
+        setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
