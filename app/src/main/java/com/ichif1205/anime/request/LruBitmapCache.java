4ecaf3b920b6c45d4e869a9e346d3929b72e59cc
mofumofu3n <mofumofu3n@gmail.com>
2014-12-13 14:35:29 +0900

    add image cache class

---
+++ app/src/main/java/com/ichif1205/anime/request/LruBitmapCache.java
@@ -0,0 +1,39 @@
+package com.ichif1205.anime.request;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache {
+
+    private static int getDefaultLruCacheSize() {
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        int cacheSize = maxMemory / 8;       // 最大メモリに依存
+
+        return cacheSize;
+    }
+
+    public LruBitmapCache() {
+        this(getDefaultLruCacheSize());
+    }
+
+    public LruBitmapCache(int sizeInKBytes) {
+        super(sizeInKBytes);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight() / 1024;
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
