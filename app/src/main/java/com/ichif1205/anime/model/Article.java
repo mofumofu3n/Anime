11bf4874791a6f1d428acf26212ab06e25234b3d
mofumofu3n <mofumofu3n@gmail.com>
2014-12-13 14:55:23 +0900

    add Article model

---
+++ app/src/main/java/com/ichif1205/anime/model/Article.java
@@ -0,0 +1,63 @@
+package com.ichif1205.anime.model;
+
+import android.text.TextUtils;
+import android.text.format.DateFormat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Article {
+    private static final String ID = "id";
+    private static final String TITLE = "title";
+    private static final String URL = "url";
+    private static final String IMAGE = "image";
+    private static final String FEED = "feed_name";
+    private static final String DATE = "published_date";
+    private static final String CLICK = "click";
+
+    private static final String DATE_FORMAT = "MM/dd kk:mm";
+
+    public String id;
+    public String title;
+    public String url;
+    public String image;
+    public String feedName;
+    public String date;
+    public int click;
+
+    public static Article parse(JSONObject obj) {
+        Article article = null;
+
+        try {
+            article = new Article();
+            article.id = obj.getString(ID);
+            article.title = obj.getString(TITLE);
+            article.url = obj.getString(URL);
+            article.image = obj.getString(IMAGE);
+            article.feedName = obj.getString(FEED);
+            article.date = article.changeDateFormat(obj.getLong(DATE));
+            article.click = article.changeNullToZero(obj.getString(CLICK));
+        } catch (JSONException e) {
+        }
+
+        return article;
+    }
+
+    /**
+     * UnixTimeを文字列に変更
+     *
+     * @param timestamp millisec
+     * @return MM/dd kk:mmにフォーマットされた日付
+     */
+    private String changeDateFormat(long timestamp) {
+        final String dateFormat = (String) DateFormat.format(DATE_FORMAT, timestamp);
+        return dateFormat;
+    }
+
+    private int changeNullToZero(String val) {
+        if (!TextUtils.equals("null", val)) {
+            return Integer.parseInt(val);
+        }
+        return 0;
+    }
+}
