cc547c7dc8423c8ff4cfcc98ba469bc343cc5d9f
mofumofu3n <mofumofu3n@gmail.com>
2014-12-13 14:43:44 +0900

    modify checkstyle warning

--- app/src/main/java/com/ichif1205/anime/request/LruBitmapCache.java
+++ app/src/main/java/com/ichif1205/anime/request/LruBitmapCache.java
@@ -6,10 +6,12 @@ import android.support.v4.util.LruCache;
 import com.android.volley.toolbox.ImageLoader;
 
 public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache {
+    private static final int KBYTE = 1024;
+    private static final int MAX_CACHE_SIZE = 8;
 
     private static int getDefaultLruCacheSize() {
-        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        int cacheSize = maxMemory / 8;       // 最大メモリに依存
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / KBYTE);
+        int cacheSize = maxMemory / MAX_CACHE_SIZE;       // 最大メモリに依存
 
         return cacheSize;
     }
@@ -24,7 +26,7 @@ public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageLoa
 
     @Override
     protected int sizeOf(String key, Bitmap value) {
-        return value.getRowBytes() * value.getHeight() / 1024;
+        return value.getRowBytes() * value.getHeight() / KBYTE;
     }
 
     @Override
