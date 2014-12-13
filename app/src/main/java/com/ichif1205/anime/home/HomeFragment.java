d0a79e021583ad39eacfa5900cb4860543f1ecb6
mofumofu3n <mofumofu3n@gmail.com>
2014-12-13 15:07:08 +0900

    change findViewId to ButterKnife

--- app/src/main/java/com/ichif1205/anime/home/HomeFragment.java
+++ app/src/main/java/com/ichif1205/anime/home/HomeFragment.java
@@ -15,9 +15,12 @@ import android.view.ViewGroup;
 import com.ichif1205.anime.R;
 import com.ichif1205.anime.list.ArticleFragment;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class HomeFragment extends Fragment {
-    private DrawerLayout mDrawerLayout;
-    private RecyclerView mDrawerList;
+    @InjectView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+    @InjectView(R.id.left_drawer) RecyclerView mDrawerList;
     private String[] mDataset;
 
     @Override
@@ -25,17 +28,22 @@ public class HomeFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.inject(this, view);
 
         mDataset = getResources().getStringArray(R.array.dataset);
 
-        mDrawerLayout = (DrawerLayout) view.findViewById(R.id.drawer_layout);
-        mDrawerList = (RecyclerView) view.findViewById(R.id.left_drawer);
         mDrawerList.setHasFixedSize(true);
         mDrawerList.setLayoutManager(new LinearLayoutManager(getActivity()));
         mDrawerList.setAdapter(new DrawerAdapter(mDataset));
 
         assignArticleFragment();
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 
     private void assignArticleFragment() {
