f97054038578890e5f045b8c2ee63d8ec958cbf2
mofumofu3n <mofumofu3n@gmail.com>
2014-12-15 02:03:37 +0900

    add tab

---
+++ app/src/main/java/com/ichif1205/anime/list/ArticleFragment.java
@@ -0,0 +1,41 @@
+package com.ichif1205.anime.list;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.ichif1205.anime.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class ArticleFragment extends Fragment{
+    @InjectView(R.id.tabs)
+    PagerSlidingTabStrip mTabs;
+
+    @InjectView(R.id.pager)
+    ViewPager mPager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_article, container, false);
+        ButterKnife.inject(this, view);
+
+        final ArticlePagerAdapter adapter = new ArticlePagerAdapter(getActivity().getSupportFragmentManager());
+
+        mPager.setAdapter(adapter);
+
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        mPager.setPageMargin(pageMargin);
+        mTabs.setViewPager(mPager);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/ichif1205/anime/list/ArticlePagerAdapter.java
@@ -0,0 +1,28 @@
+package com.ichif1205.anime.list;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ArticlePagerAdapter extends FragmentPagerAdapter {
+    private static final String[] TITLES = {"新着", "人気"};
+
+    public ArticlePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TITLES[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new NewArticleFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return TITLES.length;
+    }
+}
