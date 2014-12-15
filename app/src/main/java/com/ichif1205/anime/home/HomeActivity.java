dc4e47bf730073f6ab49558c9b6cbd8dca8f0514
mofumofu3n <mofumofu3n@gmail.com>
2014-12-16 00:32:42 +0900

    DrawerLayoutをActivityに実装

--- app/src/main/java/com/ichif1205/anime/home/HomeActivity.java
+++ app/src/main/java/com/ichif1205/anime/home/HomeActivity.java
@@ -1,17 +1,29 @@
 package com.ichif1205.anime.home;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.crashlytics.android.Crashlytics;
 import com.ichif1205.anime.R;
+import com.ichif1205.anime.list.ArticleFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class HomeActivity extends FragmentActivity {
+public class HomeActivity extends ActionBarActivity {
+    @InjectView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @InjectView(R.id.left_drawer)
+    RecyclerView mDrawerList;
+    private String[] mDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +31,13 @@ public class HomeActivity extends FragmentActivity {
         Crashlytics.start(this);
         setContentView(R.layout.activity_home);
 
-        final FragmentManager manager = getSupportFragmentManager();
-        final FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, new HomeFragment());
-        transaction.commit();
+        ButterKnife.inject(this);
+
+        setupDrawerList();
+
+        setupArticleFragment();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,5 +52,23 @@ public class HomeActivity extends FragmentActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerList() {
+        mDataset = getResources().getStringArray(R.array.dataset);
+
+        mDrawerList.setHasFixedSize(true);
+        mDrawerList.setLayoutManager(new LinearLayoutManager(this));
+        mDrawerList.setAdapter(new DrawerAdapter(mDataset));
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void setupArticleFragment() {
+        final FragmentManager manager = getSupportFragmentManager();
+        final FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, new ArticleFragment());
+        transaction.commit();
     }
 }
