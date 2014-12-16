98def7e0c84d94b38ed591251b24eab0fb7827de
mofumofu3n <mofumofu3n@gmail.com>
2014-12-17 00:36:13 +0900

    ドロワーメニュー開閉時にアイコンがアニメーションするように修正

--- app/src/main/java/com/ichif1205/anime/home/HomeActivity.java
+++ app/src/main/java/com/ichif1205/anime/home/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.ichif1205.anime.home;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -23,6 +25,8 @@ public class HomeActivity extends ActionBarActivity {
     DrawerLayout mDrawerLayout;
     @InjectView(R.id.left_drawer)
     RecyclerView mDrawerList;
+
+    private ActionBarDrawerToggle mDrawerToggle;
     private String[] mDataset;
 
     @Override
@@ -51,7 +55,7 @@ public class HomeActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     private void setupDrawerList() {
@@ -60,6 +64,9 @@ public class HomeActivity extends ActionBarActivity {
         mDrawerList.setHasFixedSize(true);
         mDrawerList.setLayoutManager(new LinearLayoutManager(this));
         mDrawerList.setAdapter(new DrawerAdapter(mDataset));
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name, R.string.app_name);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -70,5 +77,17 @@ public class HomeActivity extends ActionBarActivity {
         final FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.container, new ArticleFragment());
         transaction.commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
