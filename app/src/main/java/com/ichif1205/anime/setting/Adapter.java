5b3c1acb864306dde89c303d07db7c73a16a0af6
mofumofu3n <mofumofu3n@gmail.com>
2014-12-28 03:27:13 +0900

    リストは移動できるけど、一つ目のViewとかぶる

--- app/src/main/java/com/ichif1205/anime/setting/Adapter.java
+++ app/src/main/java/com/ichif1205/anime/setting/Adapter.java
@@ -1,7 +1,10 @@
 package com.ichif1205.anime.setting;
 
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -37,15 +40,23 @@ class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         return mDataset.length;
     }
 
-    public class Usage extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class Usage extends RecyclerView.ViewHolder implements View.OnClickListener, SwipeDelegate {
+        private final String TAG = Usage.class.getSimpleName();
+
         @InjectView(R.id.title)
         TextView titleView;
 
+        private View mItemView;
+        private Bitmap mItemViewCapture;
+        private ViewGroup.MarginLayoutParams mParams;
 
         public Usage(View itemView) {
             super(itemView);
-            ButterKnife.inject(this, itemView);
-            itemView.setOnClickListener(this);
+            mItemView = itemView;
+            ButterKnife.inject(this, mItemView);
+            mItemView.setOnClickListener(this);
+            mItemView.setOnTouchListener(moving);
+            mParams = (ViewGroup.MarginLayoutParams) mItemView.getLayoutParams();
         }
 
         private void bind(String title) {
@@ -54,7 +65,112 @@ class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         @Override
         public void onClick(View v) {
+            Log.d(TAG, "onClick");
+        }
+
+        @Override
+        public void onLeftSwipe(float distanceX) {
+            mParams.leftMargin = mParams.leftMargin + (int) distanceX;
+
+            mItemView.layout(mParams.leftMargin,
+                    mParams.topMargin,
+                    mParams.leftMargin + mItemView.getWidth(),
+                    mParams.topMargin + mItemView.getHeight());
+        }
+
+        @Override
+        public void onEndSwipe() {
 
         }
+
+        final View.OnTouchListener moving = new View.OnTouchListener() {
+
+            private float downX;
+            private float downY;
+
+            private int downLeftMargin;
+            private int downTopMargin;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                // ViewGroup.MarginLayoutParamsでキャストすることで
+                // FrameLayoutの子要素であっても同様に扱える。
+                final ViewGroup.MarginLayoutParams param =
+                        (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    downX = event.getRawX();
+                    downY = event.getRawY();
+
+                    downLeftMargin = param.leftMargin;
+                    downTopMargin = param.topMargin;
+
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+                    param.leftMargin = downLeftMargin + (int) (event.getRawX() - downX);
+                    param.topMargin = downTopMargin + (int) (event.getRawY() - downY);
+
+                    v.layout(param.leftMargin,
+                            param.topMargin,
+                            param.leftMargin + v.getWidth(),
+                            param.topMargin + v.getHeight());
+
+                    return true;
+                }
+
+                return false;
+            }
+        };
     }
+
+    public interface SwipeDelegate {
+        void onLeftSwipe(float distanceX);
+
+        void onEndSwipe();
+    }
+
+    public class LeftSwipeGestureListener implements View.OnTouchListener {
+        private final String TAG = LeftSwipeGestureListener.class.getSimpleName();
+
+        private SwipeDelegate mDelegate;
+        private float mDownX;
+
+        public LeftSwipeGestureListener(SwipeDelegate delegate) {
+            mDelegate = delegate;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (mDelegate == null) {
+                return true;
+            }
+
+            if (MotionEvent.ACTION_DOWN == event.getAction()) {
+                mDownX = event.getX();
+                return true;
+            }
+
+            if (MotionEvent.ACTION_UP == event.getAction()) {
+                mDelegate.onEndSwipe();
+                return false;
+            }
+
+            if (MotionEvent.ACTION_MOVE == event.getAction()) {
+                final float distanceX = event.getRawX() - mDownX;
+
+                if (distanceX <= 0) {
+                    return true;
+                }
+
+                mDelegate.onLeftSwipe(distanceX);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
 }
