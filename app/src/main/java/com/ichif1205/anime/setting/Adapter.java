6c17b6d559d5e00359c8e9bbb35a18274157577c
mofumofu3n <mofumofu3n@gmail.com>
2014-12-27 20:16:54 +0900

    設定画面に仮レイアウトを追加

--- app/src/main/java/com/ichif1205/anime/setting/Adapter.java
+++ app/src/main/java/com/ichif1205/anime/setting/Adapter.java
@@ -1,23 +1,60 @@
 package com.ichif1205.anime.setting;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ichif1205.anime.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final String[] mDataset;
+
+    public Adapter(String[] dataset) {
+        mDataset = dataset;
+    }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        final View view = inflater.inflate(R.layout.layout_setting, parent, false);
+
+        return new Usage(view);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        ((Usage) holder).bind(mDataset[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDataset.length;
     }
 
+    public class Usage extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @InjectView(R.id.title)
+        TextView titleView;
+
+
+        public Usage(View itemView) {
+            super(itemView);
+            ButterKnife.inject(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        private void bind(String title) {
+            titleView.setText(title);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 }
--- app/src/main/java/com/ichif1205/anime/setting/SettingFragment.java
+++ app/src/main/java/com/ichif1205/anime/setting/SettingFragment.java
@@ -3,7 +3,6 @@ package com.ichif1205.anime.setting;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +16,15 @@ public class SettingFragment extends Fragment {
     @InjectView(R.id.setting_list)
     SettingListView mListView;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_setting, container, false);
         ButterKnife.inject(this, view);
+
+        final String[] dataset = getResources().getStringArray(R.array.setting);
+        final Adapter adapter = new Adapter(dataset);
+        mListView.setAdapter(adapter);
 
         return view;
     }
