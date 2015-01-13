597855fd25134f31cec09e2b483a6d0f988c138d
mofumofu3n <mofumofu3n@gmail.com>
2015-01-14 00:40:34 +0900

    Parseのライブラリを追加

--- app/src/main/java/com/ichif1205/anime/Application.java
+++ app/src/main/java/com/ichif1205/anime/Application.java
@@ -1,5 +1,12 @@
 package com.ichif1205.anime;
 
+import com.parse.Parse;
+
 public class Application extends android.app.Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, ParseConfig.APPLICATION_ID, ParseConfig.CLIENT_KEY);
+    }
 }
---
+++ app/src/main/java/com/ichif1205/anime/ParseConfigExample.java
@@ -0,0 +1,9 @@
+package com.ichif1205.anime;
+
+/**
+ * 利用する際はファイル名、クラス名を<em>ParseConfig</em>に変更する
+ */
+public class ParseConfigExample {
+    public static final String APPLICATION_ID = "";
+    public static final String CLIENT_KEY = "";
+}
