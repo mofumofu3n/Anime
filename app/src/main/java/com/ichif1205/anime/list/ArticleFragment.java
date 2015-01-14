e8eae084abadf97bb2ee1bfd09cbee64aba7845c
mofumofu3n <mofumofu3n@gmail.com>
2015-01-14 22:33:23 +0900

    ArticlePagerAdapterをArticleFragmentのインナークラスに変更

--- app/src/main/java/com/ichif1205/anime/list/ArticleFragment.java
+++ app/src/main/java/com/ichif1205/anime/list/ArticleFragment.java
@@ -3,6 +3,8 @@ package com.ichif1205.anime.list;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -28,14 +30,40 @@ public class ArticleFragment extends Fragment {
         ButterKnife.inject(this, view);
 
         final ArticlePagerAdapter adapter = new ArticlePagerAdapter(getActivity().getSupportFragmentManager());
-
         mPager.setAdapter(adapter);
 
         final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
                 .getDisplayMetrics());
         mPager.setPageMargin(pageMargin);
         mTabs.setViewPager(mPager);
-
         return view;
+    }
+
+
+    public static class ArticlePagerAdapter extends FragmentPagerAdapter {
+        private static final String[] TITLES = {"新着", "人気"};
+
+        public ArticlePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new NewArticleFragment();
+            } else {
+                return new TodayArticleFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
     }
 }
